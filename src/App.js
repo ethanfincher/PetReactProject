16b418b3a84dec21b4db63aa950ef0012bf4cffa
ethanfincher <ethan.j.fincher@gmail.com>
2021-03-11 20:53:50 -0600

    links connected, homepage and catpage connected to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import React from 'react';
 import './App.css';
-import PetCardList from './components/PetCardList.js';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import PetPage from './components/PetPage.js';
+import HomePage from './components/HomePage.js'
 
 function App() {
   return (
-    <div className="App">
-      <PetCardList></PetCardList>
-    </div>
-  );
+		<Router>
+			<div className='App'>
+				<header>
+					<h1>
+						<Link to='/'><h1>home Page</h1></Link>
+					</h1>
+					<h1>
+						<Link to='/petPage'>
+							<h1>pet page</h1>
+						</Link>
+					</h1>
+				</header>
+				<main>
+					<Switch>
+						<Route path='/PetPage'>
+							<PetPage></PetPage>
+						</Route>
+						<Route path='/'>
+							<HomePage></HomePage>
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/PetPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PetCardList from './PetCardList'
+
+export default function PetPage() {
+    return (
+        <div>
+            <h1>this is the PetPage</h1>
+            <PetCardList></PetCardList>
+        </div>
+    )
+}
