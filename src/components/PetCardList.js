89de3b225b1671fef0a482609e5592e1a5171ca6
ethanfincher <ethan.j.fincher@gmail.com>
2021-03-12 19:17:56 -0600

    brain is busted, added some state for breed, did more research on breeds for APi, and made first API call

--- src/components/PetCardList.js
+++ src/components/PetCardList.js
@@ -1,45 +1,21 @@
-import React, { useState } from 'react'
-import PetCard from "./PetCard.js"
+import React, { useState, useEffect } from 'react';
+import PetCard from './PetCard.js';
 
 export default function PetCardList() {
-    const petList = [
-			{
-				breeds: [],
-				id: 'e75',
-				url: 'https://cdn2.thecatapi.com/images/e75.jpg',
-				width: 500,
-				height: 375,
-			},
-			{
-				breeds: [],
-				categories: [{ id: 14, name: 'sinks' }],
-				id: 'MTc5MjI3MA',
-				url: 'https://cdn2.thecatapi.com/images/MTc5MjI3MA.jpg',
-				width: 603,
-				height: 605,
-			},
-			{
-				breeds: [],
-				id: '9l6',
-				url: 'https://cdn2.thecatapi.com/images/9l6.jpg',
-				width: 720,
-				height: 960,
-			},
-			{
-				breeds: [],
-				id: 'a25',
-				url: 'https://cdn2.thecatapi.com/images/a25.jpg',
-				width: 400,
-				height: 300,
-			},
-		];
 
-        const [catList, setCatList] = useState(petList)
-            return (
-        <div>
-            {catList.map((cat) =>(
-                <PetCard cat = {cat} />
-            ))}
-        </div>
-    )
+	const [catList, setCatList] = useState([]);
+
+	// useEffect(() => {
+	// 	fetch('https://api.thecatapi.com/v1/images/search?limit=5')
+	// 		.then((response) => response.json())
+	// 		.then((data) => setCatList(data));
+	// }, []);
+
+	return (
+		<div>
+			{catList.map((cat) => (
+				<PetCard cat={cat} />
+			))}
+		</div>
+	);
 }
--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PetCardList from './PetCardList'
 
 export default function PetPage() {
+    const [breed, setBreed] = useState("")
+
     return (
-        <div>
-            <h1>this is the PetPage</h1>
-            <PetCardList></PetCardList>
-        </div>
-    )
+			<div>
+				<h1>this is the PetPage</h1>
+				<form>
+					<label for='breed'></label>
+					<select name='breed' id='breed' onChange = {(event) => setBreed
+                    (event.target.value)}>
+						<option value='abys'>Abyssinian</option>
+						<option value='abob'>American Bobtail</option>
+						<option value='asho'>American Shorthair</option>
+						<option value='beng'>Bengal</option>
+					</select>
+					<button type='submit'>search</button>
+				</form>
+				<PetCardList></PetCardList>
+			</div>
+		);
 }
